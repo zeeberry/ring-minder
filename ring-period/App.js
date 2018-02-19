03969209ce1a38b6821c622b3581c3f0ed9be8ae
zeeberry <zainabebrahimi@gmail.com>
2018-02-18 23:40:51 -0500

    remove timer and instead fix endDate

--- ring-period/App.js
+++ ring-period/App.js
@@ -1,102 +1,52 @@
 import React from 'react';
 import { Alert, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
 
-class Ring extends React.Component {
+class CurrentStatus extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isInserted: false};
+    this.state = {
+      isInserted: false,
+      datePressed: 0,
+      endDate: 0
+    };
   }
 
   _onPress = () => {
-    this.setState({ datePressed: new Date()});
-    const {isInserted, datePressed} = this.state;
-    const days = isInserted ? 21 : 7;
-    const endDate = datePressed.setDate(datePressed.getDate() + days);
-
-    this.props.onSetCountDownTimer(endDate, 1);
+    const {isInserted} = this.state;
+    const datePressed = new Date();
+    const endDate = this._getEndDate(datePressed);
+    console.log(endDate);
+    console.log(datePressed);
 
     this.setState(previousState => {
-      return { isInserted: !previousState.isInserted };
+      return { 
+        isInserted: !previousState.isInserted,
+        datePressed: datePressed, 
+        endDate: endDate
+      };
     });
+
+    console.log('state ' + JSON.stringify(this.state));
+  }
+
+  _getEndDate = (datePressed) => {
+    const {isInserted} = this.state;
+    const days = isInserted ? 21 : 7;
+    const newDate = new Date();
+    const unixEndDate = newDate.setDate(datePressed.getDate() + days);
+    const endDate = new Date(unixEndDate);
+    return endDate;
   }
 
   render() {
-    let text = this.state.isInserted ? 'Inserted' : 'Removed';
+    let currentStatus = this.state.isInserted ? 'Inserted' : 'Removed';
 
     return (
       <TouchableWithoutFeedback onPress={this._onPress}>
         <View>
-          <Text>{text}</Text>
+          <Text>{currentStatus}</Text>
         </View>
       </TouchableWithoutFeedback>
-    );
-  }
-}
-
-class CountDownTimer extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      seconds: 0,
-      minutes: 0,
-      hours: 0,
-      days: 0
-    };
-  }
-
-  tick = () => {
-
-    if (this.state.secondsRemaining > 0) {
-      this.setState({secondsRemaining: this.state.secondsRemaining - 1});
-
-      if (this.state.secondsRemaining === 0) {
-        clearInterval(this.interval);
-      }
-    }
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(this.tick, 1000);
-  }
-
-  formatSeconds(seconds) {
-    const secondsInMinute = 60;
-    return seconds % secondsInMinute;
-  }
-
-  formatMinutes(seconds) {
-    const secondsInHour = 3600;
-  }
-
-  formatTime(seconds) {
-    const secondsInDay = 86400;
-    const secondsInHour = 3600;
-    const secondsInMinute = 60;
-
-    return {
-      days: ~~(seconds / secondsInDay),
-      hours: ~~(seconds / secondsInHour),
-      minutes: ~~((seconds % secondsInHour) / secondsInMinute),
-      seconds: seconds % secondsInMinute
-    }
-    
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    const {endDate} = this.props;
-    const countDown = this.formatTime(endDate);
-    return (
-      <Text style={styles.countdown}>
-        <Text style={styles.countdownUnits}>{countDown.days} Days</Text>
-        <Text style={styles.countdownUnits}>{countDown.hours} Hrs</Text>
-        <Text style={styles.countdownUnits}>{countDown.minutes} Mins</Text>
-        <Text style={styles.countdownUnits}>{countDown.seconds} Sec</Text>
-      </Text>
     );
   }
 }
@@ -105,15 +55,13 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      endDate: 0,
-      countDownStatus: 0
+      endDate: 0
     };
   }
 
-  handleSetCountDownTimer(date, countDownStatus) {
+  handleSetCountDownTimer(date) {
     this.setState({
       endDate: date,
-      countDownStatus: countDownStatus
     });
   }
 
@@ -122,8 +70,7 @@ export default class App extends React.Component {
 
     return (
       <View style={styles.container}>
-        <CountDownTimer endDate={endDate}/>
-        <Ring onSetCountDownTimer={this.handleSetCountDownTimer.bind(this)}/>
+        <CurrentStatus onSetCountDownTimer={this.handleSetCountDownTimer.bind(this)}/>
       </View>
     );
   }
@@ -135,10 +82,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  countdown: {
-    flexDirection: 'row',
-  },
-  countdownUnits: {
   }
 });
